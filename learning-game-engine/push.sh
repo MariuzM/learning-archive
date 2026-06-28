87abfbd09e095e1d9abffb0b4d0ea83c7ed6f673
MariuzM <me@marius.dev>
2026-06-28 21:09:58 +0700

    learning-game-engine: Update push.sh

--- learning-game-engine/push.sh
+++ learning-game-engine/push.sh
@@ -6,7 +6,9 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-msg="learning-game-engine: $*"
+text="$*"
+first=$(printf '%s' "${text:0:1}" | tr '[:lower:]' '[:upper:]')
+msg="learning-game-engine: ${first}${text:1}"
 
 git add -A
 git commit -m "$msg"
